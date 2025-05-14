f42a1395a0614b4b57a742f2d9a5d1dacf33f43f
Deepu Raneesh <dr833@bath.ac.uk>
2025-05-15 04:21:52 +0530

    Chatty ready for deployment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
+
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+
+  const { theme } = useThemeStore();
+
+  console.log({ onlineUsers });
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser) {
+    return <Loader />;
+  }
+
+  return (
+    <div data-theme={theme}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <LoginPage />} />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <LoginPage />}
+        />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
